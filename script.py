971ebb0e58c0d8b9fd40b0785c2dc08725248efd
Mehryar Sadati <mehryarsadati@gmail.com>
2025-01-31 00:56:45 +0330

    reading code from file added

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import readline
+import sys
 
-from antlr4 import InputStream, CommonTokenStream
+from antlr4 import InputStream, CommonTokenStream, FileStream
 from GrammarLexer import GrammarLexer
 from GrammarParser import GrammarParser
 from Visitor import Visitor
@@ -8,6 +9,7 @@ from Visitor import Visitor
 
 def main():
     readline.read_init_file()
+
     print("Welcome!")
     print("Enter 'exit' to exit the program.")
     visitor = Visitor()
@@ -15,7 +17,7 @@ def main():
         prompt = input("> ")
         if prompt == "exit":
             break
-        input_stream = InputStream(prompt)
+        input_stream = InputStream(prompt)  # for terminal input
         lexer = GrammarLexer(input_stream)
         tokens = CommonTokenStream(lexer)
         parser = GrammarParser(tokens)
@@ -25,4 +27,16 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # Check if a file argument is provided
+    if len(sys.argv) > 1:
+        # for file input
+        input_stream = FileStream(sys.argv[1])
+        lexer = GrammarLexer(input_stream)
+        token_stream = CommonTokenStream(lexer)
+        parser = GrammarParser(token_stream)
+        tree = parser.root()
+
+        visitor = Visitor()
+        visitor.visit(tree)
+    else:
+        main()
